33a5268c7dc8ec14e903b5ab5004a0bfa8c3f4c9
Justin Turner <justin@justinpturner.com>
2020-10-04 22:01:26 -0500

    added the program that updates the image and the school txt file

---
+++ data_updater.py
@@ -0,0 +1,25 @@
+from fire import Fire
+import schedule
+import time
+
+try:
+    f = Fire()
+    hour = d.current_hour()
+    f.generate_schools_in_danger()
+    f.generate_new_image()
+    
+except Exception as e: 
+    print(f"Error: {e}")
+    time.sleep(60)
+
+while hour != f.current_hour():
+    try:
+        f = Fire()
+        hour = d.current_hour()
+        f.generate_schools_in_danger()
+        f.generate_new_image()
+        hour = f.current_hour()
+    except Exception as e: 
+        print(f"Error: {e}")
+        time.sleep(3600)
+        
